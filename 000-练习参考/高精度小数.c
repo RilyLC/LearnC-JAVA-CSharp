06483068be961d72154ac3b07f13d27ffc1868dc
RilyLC <94281898+RilyLC@users.noreply.github.com>
2021-12-01 16:55:41 +0800

    练习
    测试

---
+++ 000-练习参考/高精度小数.c
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+int main()
+{
+	int a, b, quo, mod;
+	scanf_s("%d/%d", &a, &b);
+	if (10 <= a && a < b && b < 100)
+	{	int temp = a % b;
+		a = temp * 10;
+		printf("0.");
+		for (int i = 0; i < 200; i++)
+		{	
+			quo = a / b;
+			mod = a % b;
+			printf("%d", quo);
+			if (mod == 0)
+			{
+				break;
+			}
+			a=mod * 10;
+			
+		}
+		printf("\n");
+	}
+	return 0;
+}
